c396fa86c6ddc71264d10f9600164ab51a5f555b
Matheus Rodrigues <matheus.alg10@gmail.com>
2018-05-30 15:55:38 -0300

    Tpl site

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php 
 
-require_once("vendor/autoload.php");
-use \Slim\Slim;
-use \Hcode\Page;
+//								 Configurações bases do Index 				
+	//Require dos arquivos composer
+	require_once("vendor/autoload.php");
 
+	//Usando o Slim framework para routes
+	use \Slim\Slim;
+
+	//Usando a classe Page para gerar o template completo HTML (Header, index, footer)
+	use \Hcode\Page;
+
+// 							Finalizado configurações bases do Index 			
+
+
+//Iniciando o Slim para rotas
 $app = new Slim();
 
 //Mostra os erros caso ocorra
@@ -11,8 +21,11 @@ $app->config('debug', true);
 
 //Route padrão
 $app->get('/', function() {
-	
+
+	//Chamando a classe page() sem passar parâmetros (Vai configurar um array vazio)
 	$page = new Page();
+
+	//Desenhando o nosso index
 	$page->setTpl("index");
 
 });
