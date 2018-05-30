3dcf3da65f382fcfced42f0a9f7d13158006ca24
Matheus Rodrigues <matheus.alg10@gmail.com>
2018-05-30 11:42:14 -0300

    Adicionando classe Page e removendo arquivos de Host

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use \Slim\Slim;
+use \Hcode\Page;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 //Mostra os erros caso ocorra
 $app->config('debug', true);
 
 //Route padrão
 $app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
-	echo json_encode($results);
+	
+	$page = new Page();
+	$page->setTpl("index");
 
 });
 
---
+++ views-cache/header.69d551a046753a53fb8b76d3fe77137b.rtpl.php
@@ -0,0 +1,10 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html>
+<head>
+<!-Header->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+</head>
+<body>
+    
