60938d297da9a6eff87960113c73efcbc95ccdc8
Matheus Rodrigues <matheus.alg10@gmail.com>
2018-06-20 18:58:20 -0300

    Site - Categorias

--- index.php
+++ index.php
@@ -367,6 +367,21 @@ $app->get("/admin/categories/:idcategory/delete", function($idcategory){
 
 });
 
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", array(
+		"category"=>$category->getValues(),
+		"products"=>[]
+	));
+
+});
+
 //Execute all routes
 $app->run();
 
---
+++ views-cache/categories-menu.69d551a046753a53fb8b76d3fe77137b.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/13">Android</a></li><li><a href="/categories/11">Apple</a></li><li><a href="/categories/14">Motorola</a></li><li><a href="/categories/12">Samsung</a></li>
